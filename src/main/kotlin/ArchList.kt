956da4e852f05656befc7d5f61fa995ebe005842
IsaevMilan <107324526+IsaevMilan@users.noreply.github.com>
2023-03-25 20:28:14 +0300

    Проектная №3 v2

--- src/main/kotlin/ArchList.kt
+++ src/main/kotlin/ArchList.kt
@@ -1,4 +1,3 @@
-import kotlin.system.exitProcess
 
 class ArchList : Commander() {
     private var archMenu: MutableMap<Int, Archive> = HashMap()
@@ -8,18 +7,36 @@ class ArchList : Commander() {
         val n = readLine()!!
         val a = Archive(n, NoteList())
         archMenu[(archMenu.size + 1)] = a
-        printing()
+
     }
 
     override fun command() {
-        choice = commandReader()
-        when (choice) {
-            0 -> addArch()
-            menuNumb -> exitProcess(1)
-            else -> if (archMenu.contains(choice)) archMenu[choice]?.noteMenu?.printing() else println(
-                "Архива с выбранным номером не существует"
-            )
+        for (j in 1..archMenu.size) {
+            val str = archMenu[j]?.name
+            println("$j. $str")
+        }
+    }
+    override fun printing() {
+        while (true) {
+            println("Список архивов")
+            println("0. Создать архив")
+            menuNumb = archMenu.size + 1
+            if (archMenu.isNotEmpty()) command()
+            println("${menuNumb}. Выход.")
+            choice = commandReader()
+            when (choice) {
+                0 -> addArch()
+                menuNumb -> return
+
+                else -> if (archMenu.contains(choice)) archMenu[choice]?.noteMenu?.printing() else println(
+                    "Архива с выбранным номером не существует"
+                )
+            }
         }
     }
 }
+
+
+
+
 
